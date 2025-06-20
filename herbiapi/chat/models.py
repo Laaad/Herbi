2b9479e87899ac605190d6bc43201d63461ea351
Ladan <ladan.rb@gmail.com>
2025-06-20 14:56:11 +0200

    Create `SimulatedConversation` model and its migration file

--- herbiapi/chat/models.py
+++ herbiapi/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class SimulatedConversation(models.Model):
+    response_text = models.TextField()
+    foods = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
