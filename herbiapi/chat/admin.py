4af69ff06da0ad6eda1b82bfbe66a2e3e35e9382
Ladan <ladan.rb@gmail.com>
2025-06-29 08:11:24 +0200

    Add handle to simulate conversations through admin

--- herbiapi/chat/admin.py
+++ herbiapi/chat/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import SimulatedConversation
+from django.core.management import call_command
 
 # Register your models here.
 
 class SimulatedConversationAdmin(admin.ModelAdmin):
     list_display = ['id', 'foods', 'is_veg', 'created_at']
+    actions = ['simulate_conversations']
+
+    def simulate_conversations(self, request, queryset):
+        call_command('simulate', 100, threads=50)
+        self.message_user(request, 'Simulated conversations successfully.')
+
 
 admin.site.register(SimulatedConversation, SimulatedConversationAdmin)
